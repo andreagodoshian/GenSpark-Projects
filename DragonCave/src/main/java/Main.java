f7f9b6df5adb97b77da713e3f6205aaf0c53fdff
Andrea Godoshian <94014358+godoshian@users.noreply.github.com>
2022-03-24 11:25:39 -0400

    condensed sout's

--- DragonCave/src/main/java/Main.java
+++ DragonCave/src/main/java/Main.java
@@ -4,31 +4,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Welcome To Dragon Cave!");
-        System.out.println("=======================");
-        System.out.println("\nYou are in a land full of dragons.");
-        System.out.println("In front of you, there are two caves.");
-        System.out.println("One cave houses a friendly dragon...");
-        System.out.println("the other has a mean dragon that will eat you.");
+        System.out.println("""
+                        Welcome To Dragon Cave!
+                        =======================
+                        
+                        You are in a land full of dragons
+                        In front of you, there are two caves.
+                        One cave houses a friendly dragon...
+                        the other has a mean dragon that will EAT YOU!
+                        
+                        Which cave do you choose?
+                        *~Please enter 1 or 2 below~*""");
 
-        System.out.println("\nWhich cave will you go in?");
-        System.out.println("Please enter 1 or 2: ");
-        String choice = input.nextLine();
-        int num = Integer.valueOf(choice);
-        System.out.println("You approach a cave...");
-        System.out.println("It is dark and spooky...\n");
+        int choice = input.nextInt();
 
-        if (num == 1){
-            System.out.println("A large dragon jumps in front of you!");
-            System.out.println("The dragon eats you in one bite.");
-        } else if (num == 2){
-            System.out.println("You are quickly noticed by a shiny dragon!");
-            System.out.println("This dragon is happy to share their treasure with you.");
+        System.out.println("""
+                You approach a cave...
+                It is dark and spooky...""");
+
+        if (choice == 1){
+            System.out.println("""
+                    \nA large, scary dragon jumps in front of you!
+                    The dragon eats you in one bite.
+                    This is not Pinocchio - you do not survive.
+                    RIP 💀""");
+        } else if (choice == 2){
+            System.out.println("""
+                    \nYou are quickly spotted by a beautiful, shiny dragon!
+                    This dragon is happy to share their treasure with you.
+                    Congratulations!""");
         } else {
-            System.out.println("You are lost. Better call Mr. Ballen asap.");
+            System.out.println("""
+                    \nYou are lost. Hope you don't end up in a Mr. Ballen video.
+                    [That being said, if you do survive,
+                    you should check out Mr. Ballen on YouTube!]""");
         }
-
-
-
     }
 }
