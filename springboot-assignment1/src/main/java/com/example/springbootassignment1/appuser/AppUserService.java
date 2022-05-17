748ef15ed87a0565b8e9bbf41928e7a5e8336be2
godoshian <94014358+godoshian@users.noreply.github.com>
2022-05-17 14:53:47 -0400

    added ConfirmationToken

--- springboot-assignment1/src/main/java/com/example/springbootassignment1/appuser/AppUserService.java
+++ springboot-assignment1/src/main/java/com/example/springbootassignment1/appuser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.example.springbootassignment1.appuser;
 
+import com.example.springbootassignment1.registration.token.ConfirmationToken;
+import com.example.springbootassignment1.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -14,6 +19,9 @@ public class AppUserService implements UserDetailsService {
     // lombok "all args constructor"
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // reference, for saving the token
+    private final ConfirmationTokenService confirmationTokenService;
 
     private final static String USER_NOT_FOUND_MSG =
             "Cannot find user with email %s";
@@ -29,14 +37,21 @@ public class AppUserService implements UserDetailsService {
                 .findByEmail(appUser.getEmail()).isPresent();
         if (userExists) throw new IllegalStateException("E-mail already taken!");
 
+        // Encrypt password
         String encodedPassword = bCryptPasswordEncoder
                 .encode(appUser.getPassword());
         appUser.setPassword(encodedPassword);
-
         appUserRepository.save(appUser);
 
-        // TO-DO: send confirmation token
+        // Confirmation token
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken =
+                new ConfirmationToken(token, LocalDateTime.now(),
+                        LocalDateTime.now().plusMinutes(15), appUser);
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
 
-        return "Sign-Up User was successful!";
+        // Send email
+
+        return "The token: " + token;
     }
 }
