d4ba1f55dc6bb5f150d45ccc09b69f1a36bf175d
godoshian <94014358+godoshian@users.noreply.github.com>
2022-05-17 13:11:13 -0400

    initial commit - skeleton works

---
+++ springboot-assignment1/src/main/java/com/example/springbootassignment1/appuser/AppUserService.java
@@ -0,0 +1,24 @@
+package com.example.springbootassignment1.appuser;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppUserService implements UserDetailsService {
+
+    // lombok "all args constructor"
+    private final AppUserRepository appUserRepository;
+
+    private final static String USER_NOT_FOUND_MSG =
+            "Cannot find user with email %s";
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format(USER_NOT_FOUND_MSG, email)));
+    }
+}
