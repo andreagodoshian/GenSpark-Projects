4d967049d4f446b7599e2448e5621cd9d142317d
godoshian <94014358+godoshian@users.noreply.github.com>
2022-05-18 10:30:15 -0400

    initial commit

---
+++ springboot-assignment2/src/main/java/com/example/springbootassignment2/MainController.java
@@ -0,0 +1,13 @@
+package com.example.springbootassignment2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String landingPage() {
+        return "index";
+    }
+}
---
+++ springboot-assignment2/src/main/java/com/example/springbootassignment2/SpringbootAssignment2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootassignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAssignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAssignment2Application.class, args);
+	}
+
+}
