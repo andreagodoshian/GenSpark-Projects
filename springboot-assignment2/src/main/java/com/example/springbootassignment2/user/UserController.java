bf74417cd8a2046a29a1948499d4daffe01446b3
godoshian <94014358+godoshian@users.noreply.github.com>
2022-05-18 16:46:09 -0400

    edit button works

--- springboot-assignment2/src/main/java/com/example/springbootassignment2/user/UserController.java
+++ springboot-assignment2/src/main/java/com/example/springbootassignment2/user/UserController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -24,6 +27,29 @@ public class UserController {
     @GetMapping("/users/new")
     public String showNewForm(Model model) {
         model.addAttribute("user", new User());
+        model.addAttribute("pageTitle", "Add New User");
         return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes ra) {
+        userService.save(user);
+        ra.addFlashAttribute("message", "Great Success!");
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users/edit/{id}")
+    public String showEditForm(
+            @PathVariable("id") Integer id, Model model, RedirectAttributes ra) {
+        try{
+            User user = userService.get(id);
+            model.addAttribute("user", user);
+            model.addAttribute
+                    ("pageTitle", "Edit User (ID: " + id + ")");
+            return "user_form";
+        } catch (Exception e) {
+            ra.addFlashAttribute("message", "Error!");
+            return "redirect:/users";
+        }
     }
 }
--- springboot-assignment2/src/main/java/com/example/springbootassignment2/user/UserService.java
+++ springboot-assignment2/src/main/java/com/example/springbootassignment2/user/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,4 +15,16 @@ public class UserService {
     public List<User> listAll() {
         return (List<User>) userRepository.findAll();
     }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public User get(Integer id) throws Exception {
+        Optional<User> result = userRepository.findById(id);
+        if (result.isPresent()) return result.get();
+        else throw new Exception("Couldn't find user!");
+    }
+
+
 }
