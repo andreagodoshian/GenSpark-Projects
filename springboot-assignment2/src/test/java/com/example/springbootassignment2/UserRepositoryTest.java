96e75d7350c8a9c297f80831b064543c3ca39e05
godoshian <94014358+godoshian@users.noreply.github.com>
2022-05-18 13:11:31 -0400

    added method to junit test

--- springboot-assignment2/src/test/java/com/example/springbootassignment2/UserRepositoryTest.java
+++ springboot-assignment2/src/test/java/com/example/springbootassignment2/UserRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.util.Optional;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(false)
@@ -19,15 +21,64 @@ public class UserRepositoryTest {
 
     @Test
     public void testAddNew(){
-        User user = new User();
-        user.setEmail("daria@sicksadworld.com");
-        user.setPassword("MysticSpiral");
-        user.setFirstName("Daria");
-        user.setLastName("Morgendorffer");
+        User user1 = new User();
+        user1.setEmail("daria@sicksadworld.com");
+        user1.setPassword("MysticSpiral");
+        user1.setFirstName("Daria");
+        user1.setLastName("Morgendorffer");
+        User savedUser1 = testRepo.save(user1);
 
-        User savedUser = testRepo.save(user);
-        Assertions.assertNotNull(savedUser);
-        Assertions.assertTrue(savedUser.getId() > 0);
+        Assertions.assertNotNull(savedUser1);
+        Assertions.assertTrue(savedUser1.getId() > 0);
 
+        User user2 = new User();
+        user2.setEmail("jane@sicksadworld.com");
+        user2.setPassword("IsItFallYet");
+        user2.setFirstName("Jane");
+        user2.setLastName("Lane");
+        User savedUser2 = testRepo.save(user2);
+
+        Assertions.assertNotNull(savedUser2);
+        Assertions.assertTrue(savedUser2.getId() > 0);
+
+
+    }
+
+    @Test
+    public void testListAll() {
+        Iterable<User> findAll = testRepo.findAll();
+
+        Assertions.assertNotNull(findAll);
+
+        for(User x:findAll) System.out.println(x);
+    }
+
+    @Test
+    public void testUpdate() {
+        Integer userId = 1;
+        Optional<User> optionalUser = testRepo.findById(userId);
+        User user = optionalUser.get();
+        user.setPassword("LetsGetPizza");
+        testRepo.save(user);
+
+        User updatedUser = testRepo.findById(userId).get();
+        Assertions.assertEquals("LetsGetPizza", updatedUser.getPassword());
+    }
+
+    @Test
+    public void testGet() {
+        Integer userId = 2;
+        Optional<User> optionalUser = testRepo.findById(userId);
+
+        Assertions.assertNotNull(optionalUser);
+    }
+
+    @Test
+    public void testDelete() {
+        Integer userId = 2;
+        testRepo.deleteById(userId);
+
+        Optional<User> optionalUser = testRepo.findById(userId);
+        Assertions.assertFalse(optionalUser.isPresent());
     }
 }
