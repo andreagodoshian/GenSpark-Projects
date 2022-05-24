0a2f5943657e409580bcf8ddb3c859e761063b30
godoshian <94014358+godoshian@users.noreply.github.com>
2022-05-24 16:44:18 -0400

    project set-up

---
+++ first-react-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Secret: I have a love/hate relationship with that Maroon 5 song.</h2>
+    </div>
+  );
+}
+
+export default App;
