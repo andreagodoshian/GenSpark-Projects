89ee678aa3466b0604de66bb738ec42881fa995a
godoshian <94014358+godoshian@users.noreply.github.com>
2022-05-25 11:51:08 -0400

    five yrs later, added hockey puck :)

--- first-react-app/src/components/Navbar.js
+++ first-react-app/src/components/Navbar.js
@@ -1,26 +1,37 @@
 import React from "react";
-import logo from "../logo-test.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHockeyPuck } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="container-fluid">
-       <a className="navbar-brand" href="#">Andrea Godoshian</a>
-       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-       <span className="navbar-toggler-icon"></span>
-       </button>
-       <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-            </li>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Андрия Годошьян</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <FontAwesomeIcon icon={faHockeyPuck}></FontAwesomeIcon>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Experience</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
         </ul>
-    </div>
-  </div>
-</nav>
+      </div>
+    </nav>
   )
 }
 
