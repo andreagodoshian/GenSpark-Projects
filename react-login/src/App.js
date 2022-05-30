98ab3f0cfb6b25d0613d6d088edf7afba33fed3e
godoshian <94014358+godoshian@users.noreply.github.com>
2022-05-30 09:45:26 -0400

    initializing project

---
+++ react-login/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        She will NOT be loved! Ohhhhhhhh burn!
+      </header>
+    </div>
+  );
+}
+
+export default App;
